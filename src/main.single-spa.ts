204f01f4656afa8da91eb5f509170dabf64b417f
David Gallego <dgallego@its.jnj.com>
2021-03-17 11:03:25 +0100

    First commit

---
+++ src/main.single-spa.ts
@@ -0,0 +1,24 @@
+
+import { enableProdMode, NgZone } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { singleSpaAngular } from 'single-spa-angular';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return platformBrowserDynamic().bootstrapModule(AppModule);
+  },
+  template: '<app-root />',
+  NgZone,
+});
+
+export const { bootstrap, mount, unmount } = lifecycles;
